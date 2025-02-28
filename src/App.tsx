2166d8ca059ebf83d9ad8ae5690af592ef762559
Jeroen van de Geest <jeroen@vandegeest.eu>
2025-02-28 19:17:17 +0100

    Cum.ad

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,26 @@ import './App.css'
 
 function App() {
     return (
-        <div className="bg-slate-800 w-full h-screen text-slate-200 p-4">
-            <div className="border-dotted border-2 border-violet-500 h-full rounded-lg flex">
-                <main className="m-auto">
-                    <h1 className="text-4xl text-violet-300">Welcome to the cum zone</h1>
-                    In the cum zone we cum because we can cum
+        <div className="bg-slate-800 w-full h-screen text-slate-200 text-lg p-4">
+            <div className="border-dotted border-2 border-violet-500 h-full rounded-lg flex p-4">
+                <main className="m-auto max-w-2xl">
+                    <h1 className="text-center">Welcome to the cum.ad zone</h1>
+                    <p className="text-center">In the cum zone we cum because we can cum</p>
+                    <img src="/horny.jpg" alt="The creator being fuckable AF" />
+                    <p>
+                        You probably expected there to be some ad here right? Well ofcourse there is
+                        a fucking ad here beceause this is cum.ad what the fuck did you expect. So
+                        lets start promoting some shit ye?
+                    </p>
+                    <p>Here's some cool fucking stuff:</p>
+                    <ul>
+                        <li><a href="https://www.youtube.com/watch?v=QP--kqd_yDs" target="_blank">Daddy JSchlatt being fuckable af</a></li>
+                        <li><a href="https://heeeeeeeey.com/" target="_blank">Circle ass piece of shit website</a></li>
+                        <li><a href="https://www.reddit.com/r/Piracy/comments/raxjcg/a_noobs_guide_to_obtaining_hi_res_music_along/" target="_blank">A fucking great alternative to spotify</a></li>
+                        <li><a href="https://www.stremio.com/" target="_blank">A cool ass fuck you to netflix</a></li>
+                        <li><a href="https://fedoraproject.org/" target="_blank"><b>FUCK YOU WINDOWS I HATE YOU</b></a></li>
+                        <li><a href="https://mastodon.social/explore" target="_blank">The unelonified twitter</a></li>
+                    </ul>
                 </main>
             </div>
         </div>
